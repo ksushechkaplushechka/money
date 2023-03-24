3c88252b23a8dec0cbb6c0390e0f783fa3f81c7a
ксюшечкаплюшечка <isip_k.d.porfiryeva@mpt.ru>
2023-03-24 22:45:01 +0300

    sdf

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace учет_буджета
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Date_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ExampleDtp.DisplayDateStart = new DateTime(2023, 03, 24);
+            DateTime date = Convert.ToDateTime(ExampleDtp.Text);
+            MessageBox.Show(date.ToShortDateString());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Page win = new Page();
+            win.Show();
+        }
+    }
+}
